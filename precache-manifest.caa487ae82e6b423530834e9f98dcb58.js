791f264891c2dbed7d47c9873764fb4db7798ec9
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2020-01-28 17:07:33 +0900

    Updates

---
+++ precache-manifest.caa487ae82e6b423530834e9f98dcb58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "311f641b0574db152f403c55367cbbd8",
+    "url": "/tag-for-tamago-js/index.html"
+  },
+  {
+    "revision": "604a61330284cae68e9a",
+    "url": "/tag-for-tamago-js/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a3beb2cdf2a85177e44a",
+    "url": "/tag-for-tamago-js/static/js/2.c82389e0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tag-for-tamago-js/static/js/2.c82389e0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "604a61330284cae68e9a",
+    "url": "/tag-for-tamago-js/static/js/main.e706697d.chunk.js"
+  },
+  {
+    "revision": "2714113cf3a28d68805f",
+    "url": "/tag-for-tamago-js/static/js/runtime-main.4dd731f5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tag-for-tamago-js/static/media/logo.5d5d9eef.svg"
+  }
+]);
