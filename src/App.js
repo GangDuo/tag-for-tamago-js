526d2f3862025adebb5850002a143c6345954463
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2020-01-29 11:30:30 +0900

    用紙A4サイズを上下2分割する

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
+// https://github.com/necolas/normalize.css
+import './normalize.css';
+// https://github.com/cognitom/paper-css
+import './paper.css';
 import './App.css';
+import './bootstrap.css';
+import './half-line.css';
+
+const PAPER_SIZE = "A4";
 
 function App() {
+  useEffect(() => {
+    document.body.classList.add(PAPER_SIZE);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="sheet padding-10mm d-flex flex-column justify-content-between">
+
+      {/* Write HTML just like a web page */}
+      <article>上</article>
+      <hr className="dotted-line" />
+      <article>下</article>  
+    </section>
   );
 }
 
