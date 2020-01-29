cb38de93995b585622a82a8c9ac9fa1312cee6ac
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2020-01-29 16:26:30 +0900

    A4用紙1枚あたり2件の自社定期ルート便の荷札を表示する。

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import './paper.css';
 import './App.css';
 import './bootstrap.css';
 import './half-line.css';
+import TamagoSticker from './components/TamagoSticker';
 
 const PAPER_SIZE = "A4";
 
 function App() {
   useEffect(() => {
+    document.title = '印刷プレビュー'
     document.body.classList.add(PAPER_SIZE);
   }, []);
 
@@ -19,9 +21,23 @@ function App() {
     <section className="sheet padding-10mm d-flex flex-column justify-content-between">
 
       {/* Write HTML just like a web page */}
-      <article>上</article>
+      <article>
+        <TamagoSticker shippedAt="2020.1.29"
+                       senderName="差出人名"
+                       whereToSend="差出店名"
+                       recipientName="受取人名 様"
+                       whereToReceive="受取店名"
+                       description="パソコン" />
+      </article>
       <hr className="dotted-line" />
-      <article>下</article>  
+      <article>
+        <TamagoSticker shippedAt="2020.1.29"
+                       senderName="差出人名"
+                       whereToSend="差出店名"
+                       recipientName="受取人名 様"
+                       whereToReceive="受取店名"
+                       description="パソコン" />
+      </article>
     </section>
   );
 }
---
+++ src/components/TamagoSticker.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import Stamp from './Stamp';
+
+function TamagoSticker(props) {
+  const { senderName, whereToSend, recipientName, whereToReceive, shippedAt, contents, description } = props
+
+  return (
+    <>
+      <style jsx="true">{`
+.mv-info {
+  font-size: 20px;
+}
+      `}</style>
+      <div className="row text-center">
+        <div className="col-3 border align-self-center">
+          <div className="p-2 row border-bottom">
+            <div className="col align-self-center">出荷日</div>
+          </div>
+          <div className="p-2 row"><div className="col align-self-center">
+            <Stamp top="出荷日" middle={shippedAt} bottom={senderName} />
+          </div></div>
+        </div>
+        <div className="col-6 align-self-center">
+          <b style={{fontSize: "80px"}}>たまご便</b>
+        </div>
+        <div className="col-3 border align-self-center" style={{height: "117px"}}>
+          <div className="p-2 row border-bottom">
+            <div className="col align-self-center">口数</div>
+          </div>
+          <div className="p-2 row align-self-center" style={{height: "80px"}}>
+            <div className="col align-self-center">1-1</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="row mt-5">
+        <div className="col-4 text-right">
+          <div className="row"><div className="col border-bottom mv-info">
+            <i className="cil-flag-alt"></i>&nbsp;{whereToSend}</div>
+          </div>
+          <div className="row">
+            <div className="col border-bottom mv-info">&nbsp;{senderName}</div>
+          </div>
+        </div>
+        <div className="col-4 text-center align-self-center">
+          <i className="cil-arrow-thick-right c-icon-4xl"></i>
+        </div>
+        <div className="col-4 text-right">
+          <div className="row"><div className="col border-bottom mv-info">
+            <i className="cil-flag-alt"></i>&nbsp;{whereToReceive}</div>
+          </div>
+          <div className="row">
+            <div className="col border-bottom mv-info">&nbsp;{recipientName}</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="row  border mt-5">
+        <div className="col-4 text-center align-self-center">{contents || "その他"}</div>
+        <div className="col-8 border-left">
+          <div className="row"><div className="col">
+            <i className="cil-pencil"></i>&nbsp;<b>商品名・備考</b></div>
+          </div>
+          <div className="row" style={{height: "120px"}}>
+            <div className="col">{description}</div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default TamagoSticker;
